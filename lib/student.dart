5139959fa2ac41b4e4a1ea3cd40ce7259f0f7102
aswinpradeepc <aswinpradeepc@gmail.com>
2025-01-05 18:25:00 +0530

    add basic pages and navigation

---
+++ lib/student.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StudentPage extends StatelessWidget {
+  const StudentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Student"),
+      ),
+      body: Text("Hellow"),
+    );
+  }
+}
